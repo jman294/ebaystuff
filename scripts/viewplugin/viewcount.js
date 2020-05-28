ecf18ed1d2f82f41501320638ab1e361f2598e93
Jack DuPlessis <jduplessis294@gmail.com>
2020-05-27 23:51:00 -0400

    Add plugin

---
+++ scripts/viewplugin/viewcount.js
@@ -0,0 +1,2 @@
+document.querySelectorAll('.th-title-text')[3].addEventListener('click', function () {if (window.location.href.includes('-')){window.location.href = "/sh/lst/active?sort=visitCount&action=sort";} else {window.location.href = "/sh/lst/active?sort=-visitCount&action=sort";}})
+console.log('asdf')
